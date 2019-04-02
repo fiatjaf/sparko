87b291df5af39da441deb766817dea28b56c0871
fiatjaf <fiatjaf@gmail.com>
2019-04-02 01:44:42 +0000

    fix hmacStr to match original.

--- helpers.go
+++ helpers.go
@@ -3,14 +3,18 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha256"
-	"encoding/hex"
+	"encoding/base64"
+	"regexp"
 	"time"
 )
+
+var nonLetters = regexp.MustCompile(`\W+`)
 
 func hmacStr(key, data string) string {
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(data))
-	return hex.EncodeToString(h.Sum(nil))
+	b64 := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return nonLetters.ReplaceAllString(b64, "")
 }
 
 func probeLightningd() {
